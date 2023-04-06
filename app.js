816f3ce9a48afb8c551ebb2e6c0e70efd5d95603
werdnales <werdnales@gmail.com>
2023-04-06 21:40:45 +0300

    Make better logic in updateInput()

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const buttonsContainer = document.querySelector(".buttons-container");
+const input = document.querySelector("#input");
+const operations = document.querySelector("#operations");
 const numbers = [
   "%",
   "x²",
@@ -22,8 +24,12 @@ const numbers = [
   "=",
 ];
 
-const regex = /[÷×\-\+\=]/;
-const grey = /[%x²C]$/;
+let isOperation = false;
+let sum = [];
+let lastNumber = 0;
+
+const isOrangeBtn = /[÷×\-\+\=]/;
+const isGreyBtn = /[%x²C]$/;
 
 function initButtons() {
   for (let i = 0; i < numbers.length; i++) {
@@ -37,26 +43,65 @@ function initButtons() {
 function addButtonsListeners() {
   const button = document.querySelectorAll("button");
   button.forEach((btn) => {
-    if (regex.test(btn.textContent)) {
+    if (isOrangeBtn.test(btn.textContent)) {
       btn.classList.add("orange-buttons");
     }
-    if (grey.test(btn.textContent)) {
+    if (isGreyBtn.test(btn.textContent)) {
       btn.classList.add("grey-buttons");
     }
-    btn.addEventListener("click", clickBtn);
-    btn.addEventListener("transitionend", remove);
+    btn.addEventListener("click", addClicked);
+    btn.addEventListener("transitionend", removeClocked);
   });
 }
 
-function clickBtn(e) {
+function addClicked(e) {
   this.classList.add("clicked");
+  updateInput(e.target);
 }
 
-function remove(e) {
+function removeClocked(e) {
   if (e.propertyName !== "transform") {
     return;
   }
   this.classList.remove("clicked");
+}
+
+function updateInput(e) {
+  const buttonText = e.textContent;
+  if (input.textContent === "0") input.textContent = "";
+  if (buttonText.match(/[0-9]/)) {
+    if (input.textContent === lastNumber) {
+      input.textContent = "";
+    }
+    // operations.textContent = "";
+    input.textContent += buttonText;
+    isOperation = false;
+  }
+
+  if (buttonText.match(isOrangeBtn)) {
+    if (!isOperation) {
+      lastNumber = +input.textContent;
+      sum.push(lastNumber);
+      if (buttonText === "+") {
+        input.textContent = add(sum);
+        lastNumber = input.textContent;
+      }
+      if (buttonText === "-") {
+      }
+      isOperation = true;
+    }
+    operations.textContent = `${lastNumber} ${buttonText}`;
+  }
+}
+
+function add(args) {
+  let sum = 0;
+  args.forEach((el) => (sum += el));
+  return sum;
+}
+
+function subtract(args) {
+  
 }
 
 window.onload = () => {
