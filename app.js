6855d091d97b47f7ad79354e3bccb97e7e34a0d6
werdnales <werdnales@gmail.com>
2023-04-07 15:30:29 +0300

    Add all functionality, style clear and remove btns

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-const buttonsContainer = document.querySelector(".buttons-container");
-const input = document.querySelector("#input");
-const operations = document.querySelector("#operations");
 const numbers = [
   "%",
   "x²",
@@ -23,6 +20,11 @@ const numbers = [
   ".",
   "=",
 ];
+const buttonsContainer = document.querySelector(".buttons-container");
+const input = document.querySelector("#input");
+const operations = document.querySelector("#operations");
+const clearBtn = document.querySelector("#clear");
+const removeBtn = document.querySelector("#delete");
 
 let shouldResetScreen = false;
 let currentOperation = null;
@@ -62,6 +64,9 @@ function addButtonsListeners() {
     btn.addEventListener("click", addClicked);
     btn.addEventListener("transitionend", removeClicked);
   });
+
+  clearBtn.addEventListener("click", clear);
+  removeBtn.addEventListener("click", removeNumber);
 }
 
 function addClicked(e) {
@@ -96,8 +101,27 @@ function updateInput(e) {
     shouldResetScreen = true;
   }
 
+  // Special buttons:
+  if (buttonText === "x²") {
+    square(input.textContent);
+  }
+
   if (buttonText === "=") {
     evaluate(buttonText);
+  }
+
+  if (buttonText === "C") {
+    input.textContent = "0";
+  }
+
+  if (buttonText === "±") {
+    if (input.textContent !== "0") {
+      input.textContent = -input.textContent;
+    }
+  }
+
+  if (buttonText === ".") {
+    input.textContent += ".";
   }
 }
 
@@ -115,6 +139,15 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+}
+
+function square(a) {
+  operations.textContent = `${a}² =`;
+  input.textContent = roundResult(a * a);
+}
+
+function remainder(a, b) {
+  return a % b;
 }
 
 function resetScreen() {
@@ -136,9 +169,9 @@ function evaluate(button) {
   input.textContent = roundResult(
     operate(currentOperation, firstOperand, secondOperand)
   );
-  operations.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
   if (button === "=") {
     wasEqual = true;
+    operations.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
   }
   currentOperation = null;
 }
@@ -159,9 +192,29 @@ function operate(operator, a, b) {
       return multiply(a, b);
     case "÷":
       return divide(a, b);
+    case "%":
+      return remainder(a, b);
     default:
       return null;
   }
+}
+
+function removeNumber() {
+  const main = input.textContent;
+  if (main.length > 1 && main !== "0") {
+    input.textContent = main.substring(0, main.length - 1);
+  } else {
+    input.textContent = "0";
+  }
+}
+
+function clear() {
+  input.textContent = "";
+  operations.textContent = "";
+  currentOperation = null;
+  firstOperand = "";
+  secondOperand = "";
+  wasEqual = false;
 }
 
 window.onload = () => {
